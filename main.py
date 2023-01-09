d8b6d2dadc1399be17dd71847e629d433b9a2dbd
justinlime <justinlime1999@gmail.com>
2023-01-08 19:16:49 -0600

    Updated

--- main.py
+++ main.py
@@ -7,80 +7,161 @@ start = time.time()
 wallets = []
 validators = []
 
+named_wallets={
+    "Cold Wallet" : "und1fxnqz9evaug5m4xuh68s62qg9f5xe2vzsj44l8",
+    "All Delegations" : "und1fl48vsnmsdzcv85q5d2q4z5ajdha8yu3j7wxl3",
+    "Burned" : "und1qqqqqqqqqqqqqqqqqqqqqqqqqqqqph4djz5txt",
+    "reFUND Validator" : "und1k03uvkkzmtkvfedufaxft75yqdfkfgvgm77zwm",
+    "Bitforex" : "und18mcmhkq6fmhu9hpy3sx5cugqwv6z0wrz7nn5d7",
+    "Poloniex" : "und186slma7kkxlghwc3hzjr9gkqwhefhln5pw5k26",
+}
 
 url = f'https://rest.unification.io/cosmos'
 
 params={'pagination.limit' : '1000000000'}
-
 req = requests.session()
-
 res = req.get(f'{url}/auth/v1beta1/accounts').json()
 total_wallets = int(res['pagination']['total'])
-
-res = req.get(f'{url}/auth/v1beta1/accounts',params=params).json()
-
+total = {
+    'total_wallets': total_wallets,
+    'total_spendable' : 0,
+    'total_delegations' : 0,
+    'total_unbondings' : 0,
+    'total_rewards' : 0,
+    'total_commission' : 0,
+    'grand_total' : 0,
+    }
 
 ### Gets all addresses ###
+res = req.get(f'{url}/auth/v1beta1/accounts',params=params).json()
 for r in res['accounts']:
     if r['@type'] == '/cosmos.auth.v1beta1.BaseAccount':
-        wallets.append({'rank': 0, 'address': f"{r['address']}"})
+        wallets.append({
+            'rank':0,
+            'name': 'Wallet', 
+            'address': f"{r['address']}",
+            'total':0,
+            'spendable':0, 
+            'delegations':0,
+            'unbondings':0,
+            'rewards':0,
+            })
     else:
-        wallets.append({'rank': 0, 'address': f"{r['base_account']['address']}"})
+        wallets.append({
+            'rank':0, 
+            'name': 'Wallet',
+            'address': f"{r['base_account']['address']}",
+            'total':0,
+            'spendable':0, 
+            'delegations':0,
+            'unbondings':0,
+            'rewards':0,
+            })
 
-### Gets all Validators ###
+### Gets all validators ###
+res = req.get(f'{url}/staking/v1beta1/validators',params=params).json()
+for r in res['validators']:
+    validators.append({
+        "name" : f"{r['description']['moniker']}",
+        "address": f"{r['operator_address']}",
+        "commission" : 0, 
+    })
+
+### Gets amounts for each validators commissions (including inactive) ###
+for val in validators:
+    res = req.get(f'{url}/distribution/v1beta1/validators/{val["address"]}/commission').json()
+    for com in res['commission']['commission']:
+        amount = float(com['amount'])
+        val['commission'] += amount
+        total['grand_total'] += amount
+        total['total_commission'] += amount
+
 
 i = 0
-### Gets amounts for each wallet including rewards, unbondings, redelegations, delegations ###
-for r in wallets:
+### Gets amounts for each wallet including rewards, unbondings, delegations ###
+for wallet in wallets:
     i += 1
-    
+
+    # Names known wallets
+    for x , y in named_wallets.items():
+        if y == wallet["address"]:
+            wallet["name"] = x
+
     # Adds balance
-    res = req.get(f'{url}/bank/v1beta1/balances/{r["address"]}').json()
-    if res['balances'] == []:
-        r['amount'] = 0
-    else:
-        r['amount'] = float(res['balances'][0]['amount'])
+    res = req.get(f'{url}/bank/v1beta1/balances/{wallet["address"]}').json()
+    for bal in res['balances']:
+        amount = float(bal['amount'])
+        wallet['spendable'] += amount
+        wallet['total'] += amount
+        total['grand_total'] += amount
+        total['total_spendable'] += amount
 
     # Adds rewards
-    res = req.get(f'{url}/distribution/v1beta1/delegators/{r["address"]}/rewards').json()
-    if res['total'] != []:
-        r['amount'] += float(res['total'][0]['amount'])
+    res = req.get(f'{url}/distribution/v1beta1/delegators/{wallet["address"]}/rewards').json()
+    for rew in res['total']:
+        amount = float(rew['amount'])
+        wallet['rewards'] += amount
+        wallet['total'] += amount
+        total['grand_total'] += amount
+        total['total_rewards'] += amount
 
     # Adds unbondings
-    res = req.get(f'{url}/staking/v1beta1/delegators/{r["address"]}/unbonding_delegations').json()
-    if res['unbonding_responses'] != []:
-        for unb in res['unbonding_responses']:
-            r['amount'] += float(unb['entries'][0]['balance'])
-
-    # Adds redelegations
-    res = req.get(f'{url}/staking/v1beta1/delegators/{r["address"]}/redelegations').json()
-    if res['redelegation_responses'] != []:
-        for red in res['redelegation_responses']:
-            r['amount'] += float(red['entries'][0]['balance'])
+    res = req.get(f'{url}/staking/v1beta1/delegators/{wallet["address"]}/unbonding_delegations').json()
+    for unb in res['unbonding_responses']:
+        for un in unb['entries']:
+            amount = float(un['balance'])
+            wallet['unbondings'] += amount
+            wallet['total'] += amount
+            total['grand_total'] += amount
+            total['total_unbondings'] += amount
 
     # Adds delegations
-    res = req.get(f'{url}/staking/v1beta1/delegations/{r["address"]}').json()
-    if res['delegation_responses']:
-        for dele in res['delegation_responses']:
-            r['amount'] += float(dele['balance']['amount'])
-
-    ########TO DO: Add commission too
-
-
+    res = req.get(f'{url}/staking/v1beta1/delegations/{wallet["address"]}').json()
+    for dele in res['delegation_responses']:
+        amount = float(dele['balance']['amount'])
+        wallet['delegations'] += amount
+        wallet['total'] += amount
+        total['grand_total'] += amount
+        total['total_delegations'] += amount
+            
     print(f'{total_wallets-i} Wallets Remaining')
 
 
 ### Sorts by highest amount ###
-wallets = sorted(wallets, key=lambda d: d['amount'], reverse=True)
+wallets = sorted(wallets, key=lambda d: d['total'], reverse=True)
+validators = sorted(validators, key=lambda d: d['commission'], reverse=True)
 
 ### Adds proper rank to each wallet, and formatted amount in FUND ###
 i = 0
-for r in wallets:
+for wallet in wallets:
     i += 1
-    r['rank'] = i
-    r['amount'] = f"{r['amount']/1000000000:,}"
+    wallet['rank'] = i
+    wallet['total'] = f"{round(wallet['total']/1000000000,4):,}"
+    wallet['spendable'] = f"{round(wallet['spendable']/1000000000,4):,}"
+    wallet['delegations'] = f"{round(wallet['delegations']/1000000000,4):,}"
+    wallet['unbondings'] = f"{round(wallet['unbondings']/1000000000,4):,}"
+    wallet['rewards'] = f"{round(wallet['rewards']/1000000000,4):,}"
+    
+
+for val in validators:
+    val['commission'] = f"{round(val['commission']/1000000000,4):,}"
+    
+
+total['grand_total'] = f"{round(total['grand_total']/1000000000,4):,}"
+total['total_spendable'] = f"{round(total['total_spendable']/1000000000,4):,}"
+total['total_delegations'] = f"{round(total['total_delegations']/1000000000,4):,}"
+total['total_unbondings'] = f"{round(total['total_unbondings']/1000000000,4):,}"
+total['total_rewards'] = f"{round(total['total_rewards']/1000000000,4):,}"
+total['total_commission'] = f"{round(total['total_commission']/1000000000,4):,}"
+
+info = {
+    "total" : total,
+    "wallets" : wallets,
+    "validators" : validators,   
+}
 
 with open('wallets.json', 'w') as u:
-    json.dump(wallets,u,indent=4)
+    json.dump(info,u,indent=4)
+    
 
 print(f"Completed in {(time.time()-start)/60} minutes")
